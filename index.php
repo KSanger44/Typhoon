b049ee919d0b6b5e8a59f71adf703246b50a9a0a
Kyle Sanger <ksanger@edgewood.edu>
2022-11-27 13:53:05 -0600

    bodtog

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
         <script>
             let darkToggle = document.querySelector('#darkToggle');
 
-            darkToggle.addEventListener('change', ()=> {
-              document.announcements.classList.toggle('dark');
+              darkToggle.addEventListener('change', ()=> {
+              document.body.classList.toggle('dark');
             });
         </script>
     </head>
