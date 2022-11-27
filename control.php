b4d6822c450363aed00676772f4867d37139fbfd
Kyle Sanger <ksanger@edgewood.edu>
2022-11-27 11:09:21 -0600

    semi

--- control.php
+++ control.php
@@ -81,7 +81,7 @@
               if ($table->num_rows > 0) {
                 //output each result row
                 while($row = $result->fetch_assoc()){
-                  echo "<option value =" . $row['sID'] . ">" . $row['name'] . "</option>"
+                  echo "<option value =" . $row['sID'] . ">" . $row['name'] . "</option>";
                 }
               }
                 ?>
